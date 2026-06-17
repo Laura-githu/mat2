744f10ff2a1079f148b6cf62c2ae48d62bc93f04
Laura-githu <laura.bazan.paula@escola.pr.gov.br>
2026-06-17 19:54:49 +0000

    aula 6

--- main.js
+++ main.js
@@ -14,9 +14,17 @@ for (let i = 0; i < botoes.length; i++) {
 }
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 let tempoAtual = new Date();
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+// Para cada objetivo na lista de contadores
+for (let i = 0; i < contadores.length; i++) {
+  //Calcular o tempo usando a função e associá-lo ao objetivo
+  contadores[i].textContent = calculaTempo(tempos[i]);
+}
 function calculaTempo(tempoObjetivo) {
   let tempoAtual = new Date();
   let tempoFinal = tempoObjetivo - tempoAtual;
@@ -27,6 +35,7 @@ function calculaTempo(tempoObjetivo) {
   segundos %= 60;
   minutos %= 60;
   horas %= 24;
+  if (tempoFinal > 0) {
   return (
     dias +
     " dias " +
@@ -37,4 +46,22 @@ function calculaTempo(tempoObjetivo) {
     segundos +
     " segundos"
   );
+} else {
+  return "Prazo Finalizado";
 }
+  contadores[0].textContent = calculaTempo(tempoObjetivo1);
+  contadores[1].textContent = calculaTempo(tempoObjetivo2);
+  contadores[2].textContent = calculaTempo(tempoObjetivo3);
+  contadores[3].textContent = calculaTempo(tempoObjetivo4);
+}
+function atualizaCronometro() {
+  for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+  }
+}
+function comecaCronometro() {
+  atualizaCronometro();
+  setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
